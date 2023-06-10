5bcda40077b43319a917291c406f8c0073b0a4fc
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-06-10 16:24:45 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nimrat Khalsa on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
